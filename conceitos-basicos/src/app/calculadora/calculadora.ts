fee21967e37604594e48d58f79679d0dba552f61
kaylaineasb <kaylaineasb9@gmail.com>
2025-10-29 19:38:40 -0300

    componente calculadora

---
+++ conceitos-basicos/src/app/calculadora/calculadora.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  imports: [],
+  templateUrl: './calculadora.html',
+  styleUrl: './calculadora.scss',
+})
+export class Calculadora {
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  calcularResultado(){
+    console.log("Chamando o método");
+    this.resultado = this.numero1 + this.numero2;
+  }
+}
